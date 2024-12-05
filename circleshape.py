809285835b9c2199028c3575c602b158ea9cc689
bkkcreative19 <bkkcreative16@gmail.com>
2024-12-05 17:40:33 -0600

    added bullets and ateroids

--- circleshape.py
+++ circleshape.py
@@ -20,3 +20,10 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
+
+    def collision_check(self, object_to_collide):
+        return self.position.distance_to(object_to_collide.position) <= self.radius + object_to_collide.radius
+        # if  <= object_to_collide.position.distance_to():
+        #     return True
+        # else:
+        #     return False
